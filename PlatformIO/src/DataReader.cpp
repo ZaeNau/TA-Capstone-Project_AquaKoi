e14de1a1a1faf8e2a7163c2c84c250ccce9b0098
MuhammadNaufalNI <129809864+ZaeNau@users.noreply.github.com>
2024-03-17 22:00:40 +0700

    Update DataReader.cpp

--- PlatformIO/src/DataReader.cpp
+++ PlatformIO/src/DataReader.cpp
@@ -1,14 +1,23 @@
+#include <Firebase_ESP_Client.h>
 #include <DallasTemperature.h>
 #include <MQ135.h>
 
-void readTemperatureData() {
-  float temperature = Firebase.getFloat("Sensors/Temperature");
-  Serial.print("Read Temperature: ");
-  Serial.println(temperature);
+void readTemperatureData(FirebaseData &fbdo) {
+  if (fbdo.getFloat("Sensors/Temperature")) {
+    Serial.print("Read Temperature: ");
+    Serial.println(fbdo.floatData());
+  } else {
+    Serial.println("Failed to read temperature data");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
 }
 
-void readGasAmoniaData() {
-  float ppm = Firebase.getFloat("Sensors/GasAmonia");
-  Serial.print("Read Gas Amonia (ppm): ");
-  Serial.println(ppm);
+void readGasAmoniaData(FirebaseData &fbdo) {
+  if (fbdo.getFloat("Sensors/GasAmonia")) {
+    Serial.print("Read Gas Amonia (ppm): ");
+    Serial.println(fbdo.floatData());
+  } else {
+    Serial.println("Failed to read gas ammonia data");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
 }
