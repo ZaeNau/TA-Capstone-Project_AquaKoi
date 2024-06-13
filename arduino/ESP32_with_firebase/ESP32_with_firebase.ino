6a0f8140230d911082ce0dc7b9ea210dbd1f8eae
hanafi0808 <103515895+hanafi0808@users.noreply.github.com>
2024-06-13 23:14:07 +0700

    Update ESP32_with_firebase.ino

--- arduino/ESP32_with_firebase/ESP32_with_firebase.ino
+++ arduino/ESP32_with_firebase/ESP32_with_firebase.ino
@@ -8,8 +8,8 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-#define WIFI_SSID "Kos34D"
-#define WIFI_PASSWORD "Kos34D2024"
+#define WIFI_SSID "Kos34D_Lt2"
+#define WIFI_PASSWORD "Eric2010"
 
 #define API_KEY "AIzaSyBN2McacTs5kKbfS2Lc5umzutLqZkHuQso"
 #define DATABASE_URL "https://ta-capstone-22597-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -17,31 +17,61 @@
 #define USER_EMAIL "user@gmail.com"
 #define USER_PASSWORD "password123"
 
-#define ONE_WIRE_BUS 2
-#define MQ_SENSOR 33
-#define TDS_PIN 32
-#define PH_PIN 34
-#define TURBIDITY_SENSOR_PIN A0
+#define ONE_WIRE_BUS 2  // Pin untuk sensor Suhu
+#define MQ_sensor 33    // Pin untuk sensor Gas Amonis
+#define TDS_Pin 32      // Pin untuk sensor TDS
+#define PH_PIN 34       // Pin untuk sensor PH
+#define sensorPin A0    // Pin untuk sensor kekeruhan
 
-#define RELAY_CHILLER 16
-#define RELAY_HEATER 17
-#define RELAY_WATERPUMP 18
+const int chiller = 16; // Deklarasi dan inisialisasi pin relay
+const int Heater = 17; // Deklarasi dan inisialisasi pin relay
+const int waterpump = 18; // Deklarasi dan inisialisasi pin relay
+
+#define RL 10
+#define m -0.417
+#define b 0.425
+#define Ro 13
+#define ESPADC 4096.0
+#define ESPVOLTAGE 3300
 
 #define MIN_TEMPERATURE 20
 #define MAX_TEMPERATURE 28
-#define MIN_AMONIA 0
-#define MAX_AMONIA 0.2
-#define MIN_TDS 0
-#define MAX_TDS 100
-#define MIN_PH 0
-#define MAX_PH 14
-#define MIN_TURBIDITY 0
-#define MAX_TURBIDITY 37
+#define MIN_AMONIA 0 // Ubah nilainya sesuai rentang yang diharapkan
+#define MAX_AMONIA 0.2 // Ubah nilainya sesuai rentang yang diharapkan
+#define MIN_TDS 0 // Ubah nilainya sesuai rentang yang diharapkan
+#define MAX_TDS 100 // Ubah nilainya sesuai rentang yang diharapkan
+#define MIN_PH 0 // Ubah nilainya sesuai rentang yang diharapkan
+#define MAX_PH 14 // Ubah nilainya sesuai rentang yang diharapkan
+#define MIN_TURBIDITY 0 // Ubah nilainya sesuai rentang yang diharapkan
+#define MAX_TURBIDITY 37 // Ubah nilainya sesuai rentang yang diharapkan
 
+// Inisialisasi Sensor untuk pembacaan
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-MQ135 gasSensor(MQ_SENSOR);
-DFRobot_PH phSensor;
+MQ135 gasSensor(MQ_sensor);
+DFRobot_PH ph;
+
+const int numReadings = 5; // Jumlah pembacaan untuk perhitungan rata-rata
+float readings[numReadings]; // Array untuk menyimpan pembacaan sensor
+int readIndex = 0; // Indeks pembacaan saat ini
+float total = 0; // Total pembacaan untuk perhitungan rata-rata
+float average = 0; // Nilai rata-rata pembacaan
+
+float temperature;
+float Suhu;
+float voltage;
+float corrected_pH;
+float tdsValue;
+float Amonia;
+float kekeruhan;
+
+float temperaturePercentage;
+float amoniaPercentage;
+float tdsPercentage;
+float phPercentage;
+float turbidityPercentage;
+
+int currentIndex = 0;
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -49,11 +79,26 @@ FirebaseConfig config;
 String uid;
 String databasePath;
 
+String tempPath = "/Suhu";
+String amoPath = "/Amonia";
+String tdsPath = "/Tds";
+String phPath = "/ph";
+String turbPath = "/turbidity";
+String tempPercPath = "/SuhuPercentage";
+String amoPercPath = "/AmoniaPercentage";
+String tdsPercPath = "/TdsPercentage";
+String phPercPath = "/phPercentage";
+String turbPercPath = "/turbidityPercentage";
+String parentPath;
+
 FirebaseJson json;
 
 unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 5000;
+unsigned long timerDelay = 5000; // Increased delay to 60 seconds
 
+bool signInOK = false;
+
+// Initialize WiFi
 void initWiFi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi ..");
@@ -65,33 +110,44 @@ void initWiFi() {
   Serial.println();
 }
 
+// Function declarations
+void readTemperature();
+void readAirQuality();
+void readTDS();
+void readPH();
+void readTurbidity();
+void controlRelays();
+
 void setup() {
   Serial.begin(115200);
   sensors.begin();
 
-  pinMode(RELAY_HEATER, OUTPUT);
-  pinMode(RELAY_CHILLER, OUTPUT);
-  pinMode(RELAY_WATERPUMP, OUTPUT);
-
+  pinMode(Heater, OUTPUT); // Set pin relay sebagai output
+  pinMode(chiller, OUTPUT); // Set pin chiller sebagai output
+  pinMode(waterpump, OUTPUT); // Set pin chiller sebagai output
+  
   initWiFi();
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
-  config.token_status_callback = tokenStatusCallback;
-  config.max_token_generation_retry = 5;
+  // Assign the callback function for the long running token generation task
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 0;
+  // Initialize the library with the Firebase authen and config
   Firebase.begin(&config, &auth);
-
+  // Getting the user UID might take a few seconds
   Serial.println("Getting User UID");
-  while (auth.token.uid == "") {
+  while ((auth.token.uid) == "") {
     Serial.print('.');
     delay(500);
   }
   uid = auth.token.uid.c_str();
   Serial.print("User UID: ");
   Serial.print(uid);
-
-  databasePath = "/UsersData/" + uid;
+  // Update database path
+  databasePath = "/UsersData/" + uid + "/Sensors";
 }
 
 void loop() {
@@ -114,13 +170,13 @@ void loop() {
 
     json.set("/Suhu", String(temperature));
     json.set("/SuhuPercentage", String(temperaturePercentage));
-    json.set("/Amonia", String(amonia));
+    json.set("/Amonia", String(Amonia));
     json.set("/AmoniaPercentage", String(amoniaPercentage));
     json.set("/Tds", String(tdsValue));
     json.set("/TdsPercentage", String(tdsPercentage));
-    json.set("/ph", String(pHValue));
-    json.set("/phPercentage", String(pHPercentage));
-    json.set("/turbidity", String(turbidity));
+    json.set("/ph", String(corrected_pH));
+    json.set("/phPercentage", String(phPercentage));
+    json.set("/turbidity", String(kekeruhan));
     json.set("/turbidityPercentage", String(turbidityPercentage));
 
     String jsonData;
@@ -138,9 +194,9 @@ void updateRelayStates() {
     sendDataPrevMillis = millis();
     String relayPath = databasePath + "/relayState/";
 
-    json.set("chiller", digitalRead(RELAY_CHILLER) == HIGH ? "1" : "0");
-    json.set("heater", digitalRead(RELAY_HEATER) == HIGH ? "1" : "0");
-    json.set("waterpump", digitalRead(RELAY_WATERPUMP) == HIGH ? "1" : "0");
+    json.set("chiller", digitalRead(chiller) == HIGH ? "1" : "0");
+    json.set("heater", digitalRead(Heater) == HIGH ? "1" : "0");
+    json.set("waterpump", digitalRead(waterpump) == HIGH ? "1" : "0");
 
     String jsonData;
     json.toString(jsonData);
@@ -153,63 +209,109 @@ void updateRelayStates() {
 }
 
 void controlRelays() {
-  if (temperature < MIN_TEMPERATURE) {
-    digitalWrite(RELAY_HEATER, HIGH);
-    digitalWrite(RELAY_CHILLER, LOW);
-  } else if (temperature > MAX_TEMPERATURE) {
-    digitalWrite(RELAY_CHILLER, HIGH);
-    digitalWrite(RELAY_HEATER, LOW);
+  if (Suhu > MAX_TEMPERATURE) {
+    digitalWrite(chiller, HIGH);
+    Serial.println("Chiller ON");
+  } else if (Suhu < MIN_TEMPERATURE) {
+    digitalWrite(Heater, HIGH);
+    Serial.println("Heater ON");
   } else {
-    digitalWrite(RELAY_CHILLER, LOW);
-    digitalWrite(RELAY_HEATER, LOW);
+    digitalWrite(chiller, LOW);
+    digitalWrite(Heater, LOW);
+    Serial.println("Chiller OFF");
+    Serial.println("Heater OFF");
   }
 
-  if (tdsValue < MIN_TDS || tdsValue > MAX_TDS) {
-    digitalWrite(RELAY_WATERPUMP, HIGH);
+  if (tdsValue > MAX_TDS) {
+    digitalWrite(waterpump, HIGH);
+    Serial.println("Water Pump ON");
   } else {
-    digitalWrite(RELAY_WATERPUMP, LOW);
+    digitalWrite(waterpump, LOW);
+    Serial.println("Water Pump OFF");
   }
 }
 
-void readTemperature() {
+void readTemperature(){
   sensors.requestTemperatures();
-  temperature = sensors.getTempCByIndex(0);
-  temperaturePercentage = map(temperature, MIN_TEMPERATURE, MAX_TEMPERATURE, 0, 100);
-  temperaturePercentage = constrain(temperaturePercentage, 0, 100);
-  Serial.println("Temperature: " + String(temperature));
-  Serial.println("Temperature Percentage: " + String(temperaturePercentage));
+  float temperature = sensors.getTempCByIndex(0);
+  Suhu = 1.0465 * temperature - 1.3365;
+  Serial.print("Suhu: ");
+  Serial.print(Suhu);
+  Serial.println(" °C");
+  // Calculate percentage
+  temperaturePercentage = map(Suhu, MIN_TEMPERATURE, MAX_TEMPERATURE, 0, 100);
+  Serial.print("Suhu (Persentase): ");
+  Serial.print(temperaturePercentage);
+  Serial.println("%");
+}
+
+// Custom mapping function for float values
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void readAirQuality() {
-  amonia = gasSensor.getPPM();
-  amoniaPercentage = map(amonia, MIN_AMONIA, MAX_AMONIA, 0, 100);
-  amoniaPercentage = constrain(amoniaPercentage, 0, 100);
-  Serial.println("Air Quality (Amonia): " + String(amonia));
-  Serial.println("Amonia Percentage: " + String(amoniaPercentage));
+  float VRL = analogRead(MQ_sensor) * (3.3 / 4095.0);
+  float RS = (3.3 / VRL - 1) * 10;
+  float ratio = RS / Ro;
+  float ppm = pow(10, ((log10(ratio) - b) / m));
+  total = total - readings[readIndex];
+  readings[readIndex] = ppm;
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+  average = total / numReadings;
+  Amonia = map(average, 0, 10, 0.2, 0);
+  Serial.print("Amonia ppm: ");
+  Serial.println(Amonia);
+  // Calculate percentage using the custom mapFloat function
+  amoniaPercentage = mapFloat(Amonia, MIN_AMONIA, MAX_AMONIA, 0, 100);
+  Serial.print("Amonia (Persentase): ");
+  Serial.print(amoniaPercentage);
+  Serial.println("%");
 }
 
-void readTDS() {
-  tdsValue = analogRead(TDS_PIN);
+void readTDS(){
+  int sensorValue = analogRead(TDS_Pin);
+  average = (0.6656 * sensorValue) + 69.604;
+  tdsValue = map(average, 0, 1000, 500, 0);
+  Serial.print("TDS Value: ");
+  Serial.println(tdsValue);
+  // Calculate percentage
   tdsPercentage = map(tdsValue, MIN_TDS, MAX_TDS, 0, 100);
-  tdsPercentage = constrain(tdsPercentage, 0, 100);
-  Serial.println("TDS: " + String(tdsValue));
-  Serial.println("TDS Percentage: " + String(tdsPercentage));
+  Serial.print("TDS (Persentase): ");
+  Serial.print(tdsPercentage);
+  Serial.println("%");
 }
 
-void readPH() {
-  float voltage = analogRead(PH_PIN) * 5.0 / 1024;
-  pHValue = phSensor.readPH(voltage, temperature);
-  pHPercentage = map(pHValue, MIN_PH, MAX_PH, 0, 100);
-  pHPercentage = constrain(pHPercentage, 0, 100);
-  Serial.println("pH: " + String(pHValue));
-  Serial.println("pH Percentage: " + String(pHPercentage));
+void readPH(){
+  voltage = analogRead(PH_PIN) / ESPADC * ESPVOLTAGE;
+  float phValue = ph.readPH(voltage, temperature);
+  corrected_pH = (-0.4869 * phValue) + 9.5045;
+  Serial.print("pH:");
+  Serial.println(corrected_pH, 4);
+  // Calculate percentage
+  phPercentage = map(corrected_pH, MIN_PH, MAX_PH, 0, 100);
+  Serial.print("pH (Persentase): ");
+  Serial.print(phPercentage);
+  Serial.println("%");
 }
 
-void readTurbidity() {
-  int turbidity = analogRead(TURBIDITY_SENSOR_PIN);
-  turbidityPercentage = map(turbidity, MIN_TURBIDITY, MAX_TURBIDITY, 0, 100);
-  turbidityPercentage = constrain(turbidityPercentage, 0, 100);
-  Serial.println("Turbidity: " + String(turbidity));
-  Serial.println("Turbidity Percentage: " + String(turbidityPercentage));
-}
-
+void readTurbidity(){
+  int Turbidity = analogRead(sensorPin);
+  total = total - readings[currentIndex];
+  readings[currentIndex] = Turbidity;
+  total = total + readings[currentIndex];
+  currentIndex = (currentIndex + 1) % numReadings;
+  int average = total / numReadings;
+  kekeruhan = map(average, 0, 700, 37, 0);
+  Serial.print("Kekeruhan Air : ");
+  Serial.println(kekeruhan);
+  // Calculate percentage
+  turbidityPercentage = map(kekeruhan, MIN_TURBIDITY, MAX_TURBIDITY, 0, 100);
+  Serial.print("Kekeruhan Air (Persentase): ");
+  Serial.print(turbidityPercentage);
+  Serial.println("%");
+} 
